325ce0e1dd156231e1f026223ef52c44c92b6d1a
Yurii Khimka <jw.artwork.design@gmail.com>
2022-09-21 16:30:46 +0300

    initial commit

---
+++ js/modals/Textarea.js
@@ -0,0 +1,27 @@
+export default class Textarea {
+   addTextarea(parent, classes, placeholder, id, descValue) {
+      let wrapper = document.createElement('div');
+      let area = document.createElement('textarea');
+      let description = document.createElement('span');
+
+      area.className = classes;
+      wrapper.className = 'modal-content-wrapper';
+
+      if (placeholder) {
+         area.placeholder = placeholder;
+      }
+
+      if (id) {
+         area.id = id;
+      }
+
+      if (descValue) {
+         description.textContent = descValue;
+         description.className = 'modal-item-desc';
+         wrapper.append(description);
+      }
+
+      wrapper.append(area);
+      parent.append(wrapper);
+   }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Workflow
+
+1. Class Visit - Андрей Кононенко
+2. HTML-CSS, Ajax - Юрий Химка
+3. Modals - Никита Измайлов
+
+Technologies used: HTML, CSS, JavaScript, Git, Github, BEM, OOP, Classes, Fetch, ES6 Modules.
+
+Project on GitLab: https://gitlab.com/jw.artwork.design/step-project-visits/-/tree/main/
